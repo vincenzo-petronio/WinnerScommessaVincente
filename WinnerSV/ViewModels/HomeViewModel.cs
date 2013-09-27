294b922fd3ac7edd9918a3ebc4ea0e3253828875
vipe <vincenzo.petronio@outlook.com>
2013-09-27 17:46:59 +0200

    ADD Tap su Pivot1

--- WinnerSV/ViewModels/HomeViewModel.cs
+++ WinnerSV/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,20 @@ namespace WinnerSV.ViewModels
             else
             {
                 // Code runs "for real"
+                NavToPageCommand = new RelayCommand(() =>
+                {
+                    System.Diagnostics.Debug.WriteLine("[HOMEVIEWMODEL] " + "Tapped NavToPageCommand!");
+                });
             }
+        }
+
+        /// <summary>
+        /// RelayCommand per la Navigation.
+        /// </summary>
+        public RelayCommand NavToPageCommand
+        {
+            get;
+            private set;
         }
     }
 }
