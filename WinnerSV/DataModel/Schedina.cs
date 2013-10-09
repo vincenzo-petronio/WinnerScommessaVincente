c683e261f50e8ec10be610ab09f96fdb4c3eda9e
vipe <vincenzo.petronio@outlook.com>
2013-10-09 15:44:08 +0200

    ADD vista per ospitare la schedina giocata

--- WinnerSV/DataModel/Schedina.cs
+++ WinnerSV/DataModel/Schedina.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace WinnerSV.DataModel
     /// </summary>
     public class Schedina
     {
+        [PrimaryKey]
         public string Title { get; set; }
+        //public List<Incontro> Incontri { get; set; }
     }
 }
