65a9d7d1c95a9ccc6f651b468bc61694daf14db0
vipe <vincenzo.petronio@outlook.com>
2013-10-21 17:10:29 +0200

    ADD Converter String to Visibility

---
+++ WinnerSV/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WinnerSV.Converters
+{
+    /// <summary>
+    /// Converter per eseguire la conversione da una stringa in una proprieta' Visibility.
+    /// </summary>
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // ONE-WAY Conversion!
+            return null;
+        }
+    }
+}
