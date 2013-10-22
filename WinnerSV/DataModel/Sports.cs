fb44a7a266e77f81db15ffbedbe09a9cb1cc1f00
vipe <vincenzo.petronio@outlook.com>
2013-10-22 15:24:52 +0200

    ADD License GPLv2

--- WinnerSV/DataModel/Sports.cs
+++ WinnerSV/DataModel/Sports.cs
@@ -1,4 +1,26 @@
-﻿using System.Collections.Generic;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Sports.cs">
+//    Copyright (c) 2013 Vincenzo Petronio <vincenzo.petronio"AT"gmail.com>. All rights reserved.
+// </copyright>
+// <license>
+//    This file is part of Winner - Scommessa vincente.
+//
+//    Winner - Scommessa vincente is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    Winner - Scommessa vincente is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Winner - Scommessa vincente.  If not, see <http://www.gnu.org/licenses/>.
+// </license>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
 
 namespace WinnerSV.DataModel
 {
