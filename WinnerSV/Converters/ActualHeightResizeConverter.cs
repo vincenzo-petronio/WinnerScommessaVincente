98fca2a8db1c8a678d30683d7e618e0d83d5034d
roaned <roaned2013@gmail.com>
2013-11-05 14:14:48 +0100

    ADD Gestione antemprima con animazione (versione non completa)

---
+++ WinnerSV/Converters/ActualHeightResizeConverter.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ActualHeightResizeConverter.cs">
+//    Copyright (c) 2013 Vincenzo Petronio <vincenzo.petronio"AT"gmail.com>. All rights reserved.
+// </copyright>
+// <license>
+//    This file is part of Winner - Scommessa vincente.
+//
+//    Winner - Scommessa vincente is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    Winner - Scommessa vincente is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Winner - Scommessa vincente.  If not, see <http://www.gnu.org/licenses/>.
+// </license>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Windows.Data;
+
+namespace WinnerSV.Converters
+{
+    /// <summary>
+    /// Converter per eseguire la conversione da una dimensione in una versione in percentuale.
+    /// </summary>
+    public class ActualHeightResizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((string)parameter == "1")
+            {
+                return (value != null) ? ((double)value * (6.5 * 0.1)) : value;
+            }
+            else if ((string)parameter == "2")
+            {
+                return (value != null) ? ((double)value * (5.5 * 0.1)) : value;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- WinnerSV/Views/SportsView.xaml.cs
+++ WinnerSV/Views/SportsView.xaml.cs
@@ -1,12 +1,83 @@
 ﻿using Microsoft.Phone.Controls;
+using System.Windows.Media;
 
 namespace WinnerSV.Views
 {
     public partial class SportsView : PhoneApplicationPage
     {
+
+        private double initialPosition;
+        /// <summary>
+        /// è true se la grid è visibile, false altrimenti
+        /// </summary>
+        private bool isGridUp = false;
+
         public SportsView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// gestione del Click/Tap sulla griglia con l'anteprima della schedina
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SportsGridAnteprima_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (isGridUp)
+            {
+                StoryBoardYDown.Begin();
+                isGridUp = false;
+            }
+            else
+            {
+                StoryBoardYUp.Begin();
+                isGridUp = true;
+            }
+
+            e.Handled = true;
+        }
+
+        private void SportsGridAnteprima_ManipulationStarted(object sender, System.Windows.Input.ManipulationStartedEventArgs e)
+        {
+            var ct = (CompositeTransform)this.SportsGridAnteprima.RenderTransform;
+            if (initialPosition == null || initialPosition == 0)
+            {
+                initialPosition = ct.TranslateY;
+                DoubleAnimationUp.From = initialPosition;
+                DoubleAnimationDown.To = initialPosition;
+                System.Diagnostics.Debug.WriteLine("Grid_ManipulationStarted - Initial Position: " + initialPosition);
+            }
+            e.Handled = true;
+        }
+
+        private void SportsGridAnteprima_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            var ct = (CompositeTransform)this.SportsGridAnteprima.RenderTransform;
+            ct.TranslateY += e.DeltaManipulation.Translation.Y;
+            e.Handled = true;
+        }
+
+        private void SportsGridAnteprima_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            var ct = (CompositeTransform)this.SportsGridAnteprima.RenderTransform;
+
+            // Swipe Down-Up gesture < 0 
+            if (e.TotalManipulation.Translation.Y < 0)
+            {
+                ct.TranslateY = 0;
+                //setto la grid visible
+                isGridUp = true;
+            }
+            // Swipe Up-Down gesture
+            else if (e.TotalManipulation.Translation.Y > 0)
+            {
+                ct.TranslateY = initialPosition;
+                //setto la grid invisible
+                isGridUp = false;
+            }
+
+            e.Handled = true;
         }
     }
 }
