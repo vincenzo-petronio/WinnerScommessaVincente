670ba4b313b1797f85aeaf7072b3a3ff1bea137f
roaned <roaned2013@gmail.com>
2013-11-06 10:21:59 +0100

    ADD PubCenter, rimossi ID_CAP inutili

---
+++ WinnerSV/Views/AdsControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WinnerSV.Views
+{
+    public partial class AdsControl : UserControl
+    {
+        public AdsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
